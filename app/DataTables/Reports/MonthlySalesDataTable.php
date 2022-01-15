53c67f695155af661c12432eaecff9978136cdce
DallingtonAsin <asingwire50dallington@gmail.com>
2022-01-16 01:20:40 +0300

    changes dated 16/01/2022 01:20

--- app/DataTables/Reports/MonthlySalesDataTable.php
+++ app/DataTables/Reports/MonthlySalesDataTable.php
@@ -57,8 +57,6 @@ class MonthlySalesDataTable extends DataTable
             }else{
                 $item->TotalPurchases = 0;
             }
-          
-        
         }
          return $monthly_sales;
     }
