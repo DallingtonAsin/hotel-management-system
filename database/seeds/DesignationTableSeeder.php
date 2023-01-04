55c4f695cab16f4ccbc510c158d8a5b54a420ecd
DallingtonAsin <asingwire50dallington@gmail.com>
2023-01-04 18:06:46 +0300

    changed table structure for designations to include department_id

--- database/seeds/DesignationTableSeeder.php
+++ database/seeds/DesignationTableSeeder.php
@@ -1,12 +1,41 @@
 <?php
 
-// namespace Database\Seeders;
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Designation;
-
+use Faker\Generator;
+use Illuminate\Container\Container;
 class DesignationTableSeeder extends Seeder
 {
+
+
+         /**
+     * The current Faker instance.
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    /**
+     * Create a new seeder instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->faker = $this->withFaker();
+    }
+
+    /**
+     * Get a new Faker instance.
+     *
+     * @return \Faker\Generator
+     */
+    protected function withFaker()
+    {
+        return Container::getInstance()->make(Generator::class);
+    }
     /**
      * Run the database seeds.
      *
@@ -16,31 +45,37 @@ class DesignationTableSeeder extends Seeder
     {
         Designation::create([
             "name" => "Manager",
+            "department_id" => $this->faker->randomElement([1,2,3,4,5,6]),
             "created_by" => "Olivia",
         ]);
 
         Designation::create([
             "name" => "Office Assistant",
+            "department_id" => $this->faker->randomElement([1,2,3,4,5,6]),
             "created_by" => "Olivia",
         ]);
 
         Designation::create([
             "name" => "Cashier",
+            "department_id" => $this->faker->randomElement([1,2,3,4,5,6]),
             "created_by" => "Olivia",
         ]);
 
         Designation::create([
             "name" => "House Keeper",
+            "department_id" => $this->faker->randomElement([1,2,3,4,5,6]),
             "created_by" => "Olivia",
         ]);
 
         Designation::create([
             "name" => "Receiptionist",
+            "department_id" => $this->faker->randomElement([1,2,3,4,5,6]),
             "created_by" => "Olivia",
         ]);
 
         Designation::create([
             "name" => "Computer Assistant",
+            "department_id" => $this->faker->randomElement([1,2,3,4,5,6]),
             "created_by" => "Olivia",
         ]);
     }
