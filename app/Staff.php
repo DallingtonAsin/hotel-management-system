bcca42dc7bbcb10f81612413644a39ee51cda64d
DallingtonAsin <asingwire50dallington@gmail.com>
2023-01-17 01:33:32 +0300

    included active boolean to track permission status in staff_permissions table

--- app/Staff.php
+++ app/Staff.php
@@ -18,57 +18,61 @@ class Staff extends Authenticatable
 
   protected $table = 'staff';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-      'first_name',
-      'last_name',
-      'username',
-      'gender',
-      'email',
-      'department_id', 
-      'phone_number',
-      'other_phone_number',
-      'address',
-      'nin',
-      'tin_number',
-      'nssf_number',
-      'next_of_kin',
-      'type',
-      'status',
-      'is_active',
-      'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'first_name',
+    'last_name',
+    'username',
+    'gender',
+    'email',
+    'department_id',
+    'phone_number',
+    'other_phone_number',
+    'address',
+    'nin',
+    'tin_number',
+    'nssf_number',
+    'next_of_kin',
+    'type',
+    'status',
+    'is_active',
+    'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-      'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-      'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function permissions()
-    {
-        return $this->belongsToMany(Permission::class,'staff_permissions');
-    }
-    public function hasPermission($permission)
-    {
-        return $this->permissions->contains('name', $permission);
-    }
-
-
+  public function permissions()
+  {
+    return $this->belongsToMany(Permission::class, 'staff_permissions');
   }
+
+  public function hasPermission($permissionName)
+  {
+
+    return $this->permissions()->wherePivot('permission_id', function ($query)
+    use ($permissionName) {
+      $query->select('id')->from('permissions')->where('name', $permissionName);
+    })->wherePivot('active', true)->exists();
+    
+  }
+}
