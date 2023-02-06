846a3376255ce30d5c921dad86b506cbf18637ad
DallingtonAsin <asingwire50dallington@gmail.com>
2023-02-06 11:08:35 +0300

    added guest account for testing

--- database/seeds/StaffPermissionsTableSeeder.php
+++ database/seeds/StaffPermissionsTableSeeder.php
@@ -8,23 +8,27 @@ use App\Models\StaffPermission;
 
 class StaffPermissionsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
 
-      $permissions = config('permissions');
-      foreach($permissions as $key => $permission){
-         $permission_id = Permission::where('name', $permission)->value('id');
-         StaffPermission::create([
-            'permission_id' => $permission_id,
-            'staff_id' => 1,
-             'active' => 1
-         ]);
-      }
-
+    $permissions = config('permissions');
+    foreach ($permissions as $key => $permission) {
+      $permission_id = Permission::where('name', $permission)->value('id');
+      StaffPermission::create([
+        'permission_id' => $permission_id,
+        'staff_id' => 1,
+        'active' => 1
+      ]);
+      StaffPermission::create([
+        'permission_id' => $permission_id,
+        'staff_id' => 3,
+        'active' => 1
+      ]);
     }
+  }
 }
